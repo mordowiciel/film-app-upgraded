ce08596397c9c4b6940da4be22b421cabdc85e0a
mordowiciel <szymar95@gmail.com>
2017-11-06 15:07:11 +0100

    added RV item decorator added dimens removed redundant code

--- app/src/main/java/com/example/mordowiciel/filmappupgraded/ui/home/fragment/MovieFragment.java
+++ app/src/main/java/com/example/mordowiciel/filmappupgraded/ui/home/fragment/MovieFragment.java
@@ -2,6 +2,7 @@ package com.example.mordowiciel.filmappupgraded.ui.home.fragment;
 
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
@@ -9,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.example.mordowiciel.filmappupgraded.App;
 import com.example.mordowiciel.filmappupgraded.R;
@@ -27,11 +29,8 @@ import butterknife.ButterKnife;
 
 public class MovieFragment extends Fragment implements MovieFragmentView {
 
-//    @BindView(R.id.movie_thumbnail)
-//    MovieThumbnailView mMovieThumbnailView;
-//
-//    @BindView(R.id.progress_bar)
-//    ProgressBar mProgressBar;
+    @BindView(R.id.progress_bar)
+    ProgressBar mProgressBar;
 
     @BindView(R.id.movie_recycler_view)
     RecyclerView mRecyclerView;
@@ -70,6 +69,30 @@ public class MovieFragment extends Fragment implements MovieFragmentView {
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this.getContext(), 2);
         mAdapter = new HomeActivityAdapter();
 
+        /// Anonymous class containing custom ItemDecoration to separate the cards in the view.
+        RecyclerView.ItemDecoration separator = new RecyclerView.ItemDecoration() {
+
+            int leftSpace = 15;
+            int rightSpace = 15;
+            int topSpace = 35;
+            int bottomSpace = 35;
+
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+
+                outRect.left = leftSpace;
+                outRect.right = rightSpace;
+                outRect.bottom = bottomSpace;
+
+                if (parent.getChildLayoutPosition(view) < 2) {
+                    outRect.top = topSpace;
+                } else {
+                    outRect.top = 0;
+                }
+            }
+        };
+        mRecyclerView.addItemDecoration(separator);
+
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setAdapter(mAdapter);
@@ -79,21 +102,16 @@ public class MovieFragment extends Fragment implements MovieFragmentView {
 
     @Override
     public void showMovieData(List<Movie> movieList) {
-
         mAdapter.setDataset(movieList);
-//        mMovieThumbnailView.setTitle(movie.getTitle());
-//        mMovieThumbnailView.setImageFromUrl(TheMovieDbUrlUtil.provideFullPosterUrl(movie.getPosterSubpath()));
     }
 
     @Override
     public void showLoader() {
-//        mMovieThumbnailView.setVisibility(View.GONE);
-//        mProgressBar.setVisibility(View.VISIBLE);
+        mProgressBar.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void hideLoader() {
-//        mMovieThumbnailView.setVisibility(View.VISIBLE);
-//        mProgressBar.setVisibility(View.GONE);
+        mProgressBar.setVisibility(View.GONE);
     }
 }
